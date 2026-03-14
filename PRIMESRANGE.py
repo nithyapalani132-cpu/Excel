b1d36259a12a62c57a0799850cdb87cb97d63921
Nithya.P <nithyapalani132@gmail.com>
2026-03-14 14:47:19 +0530

    "UPLOADED PRIMESRANGE.PY"

---
+++ PRIMESRANGE.py
@@ -0,0 +1,12 @@
+def primes_in_range(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if num > 1:
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    break
+            else:
+                primes.append(num)
+    return primes
+
+print(primes_in_range(10, 50))
